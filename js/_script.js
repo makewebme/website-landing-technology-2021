64543edf98aecaae7200d1d7519d192b92a4e6f2
makewebme <makewebme@gmail.com>
2020-05-24 17:15:43 +0300

    Начало проекта

---
+++ js/_script.js
@@ -0,0 +1,1 @@
+require('../css/_style.css')
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+
+module.exports = {
+  entry: {
+    script: './js/_script.js'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  plugins: [
+    // Generating HTML
+    new HtmlWebpackPlugin({ template: 'pug/_index.pug', filename: 'index.html' }),
+    new HtmlWebpackPugPlugin(),
+
+    new MiniCssExtractPlugin({ filename: `[name].css` }), // Generating CSS
+    new CopyWebpackPlugin([{ from: 'img', to: 'img' }]) // Copy images
+  ],
+
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  },
+
+  module: {
+    rules: [
+      // HTML
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+
+      // CSS
+      {
+        test: /\.css$/,
+        use: [
+          // Extract to external CSS file
+          { loader: MiniCssExtractPlugin.loader },
+
+          // Regular CSS
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+              url: false
+            }
+          },
+
+          // PostCSS with plugins
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-nested'),
+                require('postcss-flexbugs-fixes'),
+                require('postcss-preset-env')({
+                  autoprefixer: {
+                    flexbox: 'no-2009',
+                  },
+                  stage: 3,
+                })
+              ],
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  // Development server
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    port: 4000,
+    writeToDisk: true
+  },
+
+  mode: process.env.NODE_ENV || 'development'
+}
